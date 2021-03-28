d67cfce91465a5e59128aafa1ff2a2edc67b66c1
Napopey <coolfire24@mail.ru>
2021-03-28 19:14:44 +0300

    added shop and shop2

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,30 @@
 $(function name(params) {
-  
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6", 
+    ratedFill: "#ffcc00",
+    spacing: "4px",
+  });
+  $(".star-item").rateYo({
+    starWidth: "20px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "10px",
+  });
+
+  $(".filter-price__input").ionRangeSlider({
+    type: 'double',
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true,
