f42c0d20b8bba84981448f8a8ef707ac27f91aab
Napopey <coolfire24@mail.ru>
2021-03-29 17:01:00 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function name(params) {
 
+  $('.product-one__input').styler();
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    focusOnSelect: true,
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
   $(".star").rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6", 
@@ -11,7 +29,7 @@ $(function name(params) {
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     spacing: "10px",
-  });
+  }); 
 
   $(".filter-price__input").ionRangeSlider({
     type: 'double',
