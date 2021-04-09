59860d7fbc100455f6baca31b498cdae49e132b7
Napopey <coolfire24@mail.ru>
2021-04-09 13:31:11 +0300

    added adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function name(params) {
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
   $('.product-list__items').slick({
     infinite: true,
     slidesToShow: 4,
